20980105af546c8a1eafc6e24ad2276680a3bd46
Тамара <tomka1403200340@gmail.com>
2022-09-25 12:19:21 +0300

    session 1 completed

---
+++ src/session1_Introduction/MyFirst.java
@@ -0,0 +1,13 @@
+package session1_Introduction;
+import java.lang.*; // импортируемая библиотека
+
+public class MyFirst {
+    // public - метод виден вне класса
+    // static - метод виден вне класса, но использование не влечет создание объеккта
+    public static void main (String[] arg) {
+        // println - метод, который выводит что-то на экран, следовательно, нужны скобки
+        // out - метод
+        // System - класс
+        System.out.println("hello world");
+    }
+}
